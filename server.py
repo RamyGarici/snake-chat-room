83003ee1935efba96575bb039141ec5817896f25
RamyGarici <ramymanilgarici@gmail.com>
2026-06-20 11:50:34 +0100

    handling multiple clients + broadcasting messages

--- server.py
+++ server.py
@@ -1,16 +1,42 @@
 import socket
 import threading
-
+import json
 
 HOST = '127.0.0.1'
 PORT = 4444
 
+rooms = {}
+
+
+def broadcast(room_name, message, sender_socket=None):
+    data = json.dumps({"text":message}).encode('utf-8')
+    for client in rooms[room_name]:
+        if client!= sender_socket:
+            client.send(data)
+        
+
+
 def handle_client(client_socket):
     while True:
-        data = client_socket.recv(1024)
+        data = client_socket.recv(1024).decode('utf-8')
         if not data:
             break
-        client_socket.sendall(data)
+        message = json.loads(data)
+        action = message.get("action")
+        if action == "join":
+            username = message.get("username")
+            current_room = message.get("room")
+            if current_room not in rooms:
+                rooms[current_room]={}
+            rooms[current_room][client_socket]= username
+            broadcast(current_room,message,client_socket)
+        elif action == "message":
+            message_text = message.get("text")
+            broadcast(current_room,f"{username}: {message_text}",client_socket)
+
+
+
+
 
 
 
